efb3dd21f5f7299080a05caa93c54a9160d376d4
saurzv <saurav.raj5592+git@gmail.com>
2021-08-17 17:08:23 +0530

    Added Count Sort

---
+++ count_sort.cpp
@@ -0,0 +1,45 @@
+// Counting Sort
+// Sort the given array into increasing order
+// T(n) = O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void count_sort(vector<int>& arr){
+
+    // finds the maximum number in the array
+    int max = arr[0];
+    for(auto x : arr)
+        if(x > max)
+            max = x;
+    
+    // make another array to store the count of each number in the original array
+    vector<int> c(max);
+    for (int i = 0; i < arr.size(); i++)
+        c[arr[i]] += 1;     // increase the count of each number
+    
+    // modify the array according to the count of each number
+    int j = 0;
+    for (int i = 0; i <= c.size(); i++)
+    {
+        int k = c[i];
+        for (int l = 0; l < k; l++)
+        {
+            arr[j++] = i;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {0,5,1,2,4,5,2,1,2,3};
+    count_sort(arr);
+    for(auto x : arr)
+        cout << x << " ";
+    return 0;
+}
+
+/*
+works fine
+prints : 0 1 1 2 2 2 3 4 5 5
+*/
